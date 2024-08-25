6dec652c8c8afb20cac535a616f0572a7aef752d
Eirini Tipidou <68666842+Raven-Ir@users.noreply.github.com>
2024-08-25 22:40:19 +0200

    Commit 001 - Project Set Up

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <h1>Listed</h1>
+    <p>For your daily quick notes that you might want to share...</p>
+      <a target="_blank" href="https://icons8.com/icon/67582/list">List</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+    </>
+    
+
+  )
+}
+
+// https://coolors.co/31afd4-0b4f6c-208aeb-4a2545-e0e0e2
+
+export default Home
